893d408ff2961c57abfe2bc30ecf41a2d2ef1a36
Saadman <sssoumo@gmail.com>
2019-03-07 23:30:31 +0600

    login feature added

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
+const cookieParser = require('cookie-parser'); 
 
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE)
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -30,6 +29,23 @@ app.post('/api/users/register',(req,res)=>{
         res.status(200).json({
             success: true,
             userdata: doc
+        })
+    })
+})
+
+app.post('/api/users/login',(req,res)=>{
+    User.findOne({'email':req.body.email},(err,user)=>{
+        if(!user) return res.json({loginSuccess:false,message:'Auth failed, email not found'});
+
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:'Wrong password'});
+
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth',user.token).status(200).json({
+                    loginSuccess: true
+                })
+            })
         })
     })
 })
