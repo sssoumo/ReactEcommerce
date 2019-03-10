eae41193dff0163edaf75fefd8051fe13abc8e6f
Saadman <sssoumo@gmail.com>
2019-03-10 18:56:35 +0600

    server catergory CR added

--- server/server.js
+++ server/server.js
@@ -14,12 +14,59 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 //Models
-const { User } = require('./models/user')
-
+const { User } = require('./models/user');
+const { Brand } = require('./models/brand');
+const { Wood } = require('./models/wood');
 
 //Middlewares
 const { auth } = require('./middleware/auth')
+const { admin } = require('./middleware/admin')
 
+//========================================
+//               WOODS
+//========================================
+
+app.post('/api/product/wood',auth,admin,(req,res)=>{
+    const wood = new Wood(req.body);
+
+    wood.save((err,doc)=>{
+        if(err) return res.json({success:false,err})
+        res.status(200).json({
+            success:true,
+            wood:doc
+        })
+    })
+})
+
+app.get('/api/product/woods',(req,res)=>{
+    Wood.find({},(err,woods)=>{
+        if(err) return res.status(400).send(err)
+        res.status(200).send(woods)
+    })
+})
+
+//========================================
+//               BRAND
+//========================================
+
+app.post('/api/product/brand',auth,admin,(req,res)=>{
+    const brand = new Brand(req.body);
+
+    brand.save((err,doc)=>{
+        if(err) return res.json({success:false,err})
+        res.status(200).json({
+            success:true,
+            brand:doc
+        })
+    })
+})
+
+app.get('/api/product/brands',(req,res)=>{
+    Brand.find({},(err,brands)=>{
+        if(err) return res.status(400).send(err)
+        res.status(200).send(brands)
+    })
+})
 
 //========================================
 //               USERS
@@ -65,6 +112,19 @@ app.post('/api/users/login',(req,res)=>{
             })
         })
     })
+})
+
+app.get('/api/user/logout',auth,(req,res)=>{
+    User.findOneAndUpdate(
+        {_id:req.user.id},
+        { token: '' },
+        (err,doc)=>{
+            if(err) return releaseEvents.json({success:false,err})
+            return res.status(200).send({
+                success:true
+            })
+        }
+    )
 })
 
 const port = process.env.PORT || 3002;
