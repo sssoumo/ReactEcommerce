2db26c492158e1ee26dfed2999296698ed0a92fd
Saadman <sssoumo@gmail.com>
2019-03-12 22:07:21 +0600

    product section added

--- server/server.js
+++ server/server.js
@@ -17,10 +17,48 @@ app.use(cookieParser());
 const { User } = require('./models/user');
 const { Brand } = require('./models/brand');
 const { Wood } = require('./models/wood');
+const { Product } = require('./models/product');
 
 //Middlewares
 const { auth } = require('./middleware/auth')
 const { admin } = require('./middleware/admin')
+
+//========================================
+//               PRODUCTS
+//========================================
+
+app.get('/api/product/articles_by_id',auth,admin,(req,res)=>{
+    let type = req.query.type;
+    let items = req.query.id;
+
+    if(type === "array"){
+        let ids = req.query.id.split(',');
+        items = [];
+        items = ids.map(item=>{
+            return mongoose.Types.ObjectId(item)
+        })
+    }
+    Product.
+    find({'_id':{$in:items}}).
+    populate('brand').
+    populate('wood').
+    exec((err,docs)=>{
+        return res.status(200).send(docs)
+    })
+
+})
+
+app.post('/api/product/article',auth,admin,(req,res)=>{
+    const product = new Product(req.body);
+
+    product.save((err,doc)=>{
+        if(err) return res.json({success:false,err})
+        res.status(200).json({
+            success:true,
+            article:doc
+        })
+    })
+})
 
 //========================================
 //               WOODS
