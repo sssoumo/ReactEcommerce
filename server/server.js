b611b913f9e708788aa859dc2cbbd7278006a465
Saadman <sssoumo@gmail.com>
2019-03-16 19:24:56 +0600

    client files added

--- server/server.js
+++ server/server.js
@@ -27,6 +27,27 @@ const { admin } = require('./middleware/admin')
 //               PRODUCTS
 //========================================
 
+app.get('/api/product/articles',auth,admin,(req,res)=>{
+    let order = req.query.order? req.query.order: 'asc';
+    let sortBy = req.query.sortBy? req.query.sortBy: "_id";
+    let limit = req.query.limit? parseInt(req.query.limit): 100;
+
+    
+    Product.
+    find().
+    populate('brand').
+    populate('wood').
+    sort([[sortBy,order]]).
+    limit(limit).
+    exec((err,articles)=>{
+        if(err) return res.status(400).send(err)
+        res.send(articles)
+    })
+
+})
+
+
+/// /api/product/article?id=HSHSHHSHK,JSJSJSJJS,SDHSJKIJES&type=array
 app.get('/api/product/articles_by_id',auth,admin,(req,res)=>{
     let type = req.query.type;
     let items = req.query.id;
