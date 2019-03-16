cd7ba3e5d2add95d84d57da83f34e9c663a738b6
Saadman <sssoumo@gmail.com>
2019-03-16 20:49:16 +0600

    necesary configurations done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+
+
 class App extends Component {
+
+  componentDidMount(){
+    axios.get('/api/product/brands').then(response=>{
+      console.log(response)
+    })
+  }
+
   render() {
     return (
       <div className="App">
