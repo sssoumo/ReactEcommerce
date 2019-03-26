62e444f1b86788aedb7dbc04f27ea323cdce53b9
Saadman <sssoumo@gmail.com>
2019-03-26 14:40:37 +0600

    login field created

--- client/src/components/Register_login/index.js
+++ client/src/components/Register_login/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MyButton from '../utils/button';
+import Login from './login';
 
 const RegisterLogin = () => {
     return (
@@ -23,7 +24,7 @@ const RegisterLogin = () => {
                     <div className="right">
                         <h2>Registered Customers</h2>
                         <p>If you have an account please log in.</p>
-                        LOGIN
+                        <Login />
                     </div>
                 </div>
             </div>
